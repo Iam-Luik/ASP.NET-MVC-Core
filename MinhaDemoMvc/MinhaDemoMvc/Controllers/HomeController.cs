a0dbe4f7a2a0193fb21c76c2623151f20cf7a0e6
iam-luik <castro.luik@hotmail.com>
2021-12-12 10:27:01 -0300

    model, validação de modelos e DataAnnotations

--- MinhaDemoMvc/MinhaDemoMvc/Controllers/HomeController.cs
+++ MinhaDemoMvc/MinhaDemoMvc/Controllers/HomeController.cs
@@ -4,8 +4,7 @@ using System.Diagnostics;
 
 namespace MinhaDemoMvc.Controllers
 {
-    [Route("")]
-    [Route("gestao-clientes")]
+
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
@@ -15,23 +14,37 @@ namespace MinhaDemoMvc.Controllers
         {
             _logger = logger;
         }
-        [Route("")]
-        [Route("pagina-inicial")]
-        [Route("pagina-inicial/{id:int}/{categoria:guid}")]
-        public IActionResult Index(int id, Guid categoria)
+     
+        public IActionResult Index()
         {
-            return View();
+            var filme = new Filme
+            {
+                Titulo = "Oi",
+                DataLancamento = DateTime.Now,
+                Genero = null,
+                Avaliacao = 10,
+                Valor = 20000
+            };
+            return RedirectToAction("Privacy", filme);
+            //return View();
         }
 
-        [Route("privacidade")]
-        [Route("politica-de-privacidade")]
-        public IActionResult Privacy()
+        public IActionResult Privacy(Filme filme)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+
+            }
+
+            foreach (var error in ModelState.Values.SelectMany(m => m.Errors))
+            {
+                Console.WriteLine(error.ErrorMessage);
+            }
+
+            return View(); ;
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        [Route("erro-encontrado")]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
