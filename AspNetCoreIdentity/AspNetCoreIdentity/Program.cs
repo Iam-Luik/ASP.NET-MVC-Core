55678482aef563d980e1350e70765c1edf632d24
iam-luik <castro.luik@hotmail.com>
2022-01-14 08:51:04 -0300

    Tratamento de erros na aplicação

--- AspNetCoreIdentity/AspNetCoreIdentity/Program.cs
+++ AspNetCoreIdentity/AspNetCoreIdentity/Program.cs
@@ -27,7 +27,8 @@ var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler("/erro/500");
+    app.UseStatusCodePagesWithRedirects("/erro/{0}");
     app.UseHsts();
 }
 
